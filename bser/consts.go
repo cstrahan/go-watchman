1a41a5af26194c8f9175b5ac44e12b69d3b1e623
Charles Strahan <charles.c.strahan@gmail.com>
2014-07-10 14:36:57 -0400

    extract consts

---
+++ bser/consts.go
@@ -0,0 +1,31 @@
+package bser
+
+import "unsafe"
+
+const (
+	binaryMarker = "\x00\x01"
+
+	arrayMarker    = 0x00
+	mapMarker      = 0x01
+	stringMarker   = 0x02
+	int8Marker     = 0x03
+	int16Marker    = 0x04
+	int32Marker    = 0x05
+	int64Marker    = 0x06
+	floatMarker    = 0x07
+	trueVal        = 0x08
+	falseVal       = 0x09
+	nilVal         = 0x0a
+	templateMarker = 0x0b
+	skipMarker     = 0x0c
+
+	int8Size    = int(unsafe.Sizeof(int8(0)))
+	int16Size   = int(unsafe.Sizeof(int16(0)))
+	int32Size   = int(unsafe.Sizeof(int32(0)))
+	int64Size   = int(unsafe.Sizeof(int64(0)))
+	float64Size = int(unsafe.Sizeof(float64(0)))
+
+	binaryMarkerSize = int(unsafe.Sizeof(binaryMarker))
+	sniffBufferSize  = binaryMarkerSize + int8Size
+	peekBufferSize   = sniffBufferSize + int64Size
+)
